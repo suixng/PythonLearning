1a26665fcc86e45755f78cb5c0c8ea8457714d75
suixng <suixingjun@gmail.com>
2021-04-11 21:40:29 +0800

    done

--- neural_network/learn/KNN.py
+++ neural_network/learn/KNN.py
@@ -294,7 +294,7 @@ def img2vector(filename):
     returnVect = zeros((1,1024))
     fr = open(filename)
     for i in range(32):
-        listStr = fr.readline()
+        lineStr = fr.readline()
         for j in range(32):
             returnVect[0,32*i+j] = int(lineStr[j])
     return returnVect
@@ -303,7 +303,7 @@ def img2vector(filename):
 def handwritingClassTest():
     # 1. 导入数据
     hwLabels = []
-    trainingFileList = listdir('data/2.KNN/trainingDigits')  # load the training set
+    trainingFileList = listdir('F:\\class\\neural_network\\digits\\trainingDigits')  # load the training set
     m = len(trainingFileList)
     trainingMat = zeros((m, 1024))
     # hwLabels存储0～9对应的index位置， trainingMat存放的每个位置对应的图片向量
@@ -313,17 +313,17 @@ def handwritingClassTest():
         classNumStr = int(fileStr.split('_')[0])
         hwLabels.append(classNumStr)
         # 将 32*32的矩阵->1*1024的矩阵
-        trainingMat[i, :] = img2vector('data/2.KNN/trainingDigits/%s' % fileNameStr)
+        trainingMat[i, :] = img2vector('F:\\class\\neural_network\\digits\\trainingDigits/%s' % fileNameStr)
 
     # 2. 导入测试数据
-    testFileList = listdir('data/2.KNN/testDigits')  # iterate through the test set
+    testFileList = listdir('F:\\class\\neural_network\\digits\\testDigits')  # iterate through the test set
     errorCount = 0.0
     mTest = len(testFileList)
     for i in range(mTest):
         fileNameStr = testFileList[i]
         fileStr = fileNameStr.split('.')[0]  # take off .txt
         classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('data/2.KNN/testDigits/%s' % fileNameStr)
+        vectorUnderTest = img2vector('F:\\class\\neural_network\\digits\\testDigits/%s' % fileNameStr)
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
         print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
         if (classifierResult != classNumStr): errorCount += 1.0
@@ -345,6 +345,6 @@ def classifyPerson():
 
 
 if __name__=='__main__':
-    test1()
-    datingClassTest()
-    #handwritingClassTest()
+    #test1()
+    #datingClassTest()
+    handwritingClassTest()
